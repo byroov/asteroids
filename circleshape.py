38b0824a07c733d6ce701db388d8cb6a5b6173d3
byroov <byronverheyen@gmail.com>
2025-11-16 22:52:26 -0500

    added player.py, .gitignore, and update main.py, constants.py, and circleshape.py

--- circleshape.py
+++ circleshape.py
@@ -1,4 +1,6 @@
 import pygame
+import constants
+
 
 # Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
@@ -14,8 +16,8 @@ class CircleShape(pygame.sprite.Sprite):
         self.radius = radius
 
     def draw(self, screen):
-        # must override
-        pass
+        pygame.draw.polygon(screen, "white", self.triangle(), constants.LINE_WIDTH)
+        
 
     def update(self, dt):
         # must override
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+import constants
 from logger import log_state
+from player import Player
+import circleshape
 
 
 pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 dt = 0
+SCREEN_HEIGHT = constants.SCREEN_HEIGHT
+SCREEN_WIDTH = constants.SCREEN_WIDTH
+LINE_WIDTH = constants.LINE_WIDTH
+PLAYER_RADIUS = constants.PLAYER_RADIUS
 
-
-
+player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 running = True
 while running:
@@ -19,12 +24,25 @@ while running:
             running = False
     dt = clock.tick(60) / 1000
     screen.fill("black")
+    player.draw(screen)
     pygame.display.flip()
+
+player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+class Player(player.Player):
+ def __init__(self, x, y):
+     super().__init__(x, y, PLAYER_RADIUS)
+     x = constants.SCREEN_WIDTH / 2
+     y = constants.SCREEN_HEIGHT / 2
+
+
+
+
 
 def main():
     print("Starting Asteroids with pygame version: VERSION")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
